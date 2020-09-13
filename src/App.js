cd027e0b556617cfbbfea68acd730058d455c3ba
krabecb <60526718+krabecb@users.noreply.github.com>
2020-09-12 21:30:16 -0500

    Created state for form input. Created state with an empty array. User can input data into form and submit it into todos array. Form input state clears after user submits

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Form from './components/Form'
 import ToDoList from './components/ToDoList'
 
 function App() {
-  
+  const [inputText, setInputText] = useState("")
+  const [todos, setTodos] = useState([])
   return (
     <div className="App">
       <header>
         <h1>To-do List</h1>
       </header>
-      <Form />
+      <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText} />
       <ToDoList />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,28 @@
 import React from 'react'
 
-function Form() {
+function Form(props) {
+    //Here before the return you can write JS code and functions
+    const inputTextHandler = (e) => {
+        console.log(e.target.value)
+        props.setInputText(e.target.value)
+    }
+    const submitTodoHandler = (e) => {
+        //Prevents page from refreshing when we hit enter to submit input   
+        e.preventDefault()
+        props.setTodos([
+            //...todos: If there are already elements in the array, include them as well 
+            //Removed ...todos for right now. May only be able to do ...todos if you omit typing in props by
+            //changing the form function to const Form = ({setInputText, todos, setTodos}) => {}
+            //{setInputText, todos, setTodos} lets you bypass writing props everytime
+            {text: props.inputText, completed: false}
+        ])
+        //Clear state string data
+        props.setInputText('')
+    }
     return(
         <form>
-            <input type="text" className="todo-input" />
-            <button className="todo-button" type="submit">
+            <input onChange={inputTextHandler} type="text" className="todo-input" />
+            <button onClick={submitTodoHandler} className="todo-button" type="submit">
                 <i className="fas fa-plus-square"></i>
             </button>
             <div className="select">
