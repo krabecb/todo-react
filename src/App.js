6713c08f6bf9ba4b954c943c60d78f5d130105b0
krabecb <60526718+krabecb@users.noreply.github.com>
2020-09-12 19:28:08 -0500

    Imported CSS for tutorial

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <header>
+        <h1>To-do List</h1>
+      </header>
+      
     </div>
   );
 }
