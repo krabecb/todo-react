1010753085e0ef5147173508f5ba325c46c1e776
krabecb <60526718+krabecb@users.noreply.github.com>
2020-09-13 01:17:01 -0500

    Clicking on the checkmark changes completed to true and crosses out the element in the array

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,10 +6,20 @@ function Todo(props) {
         props.setTodos(props.todos.filter((el) => el.id !== props.todo.id))
         // console.log(props.todo)
     }
+    const completeHandler = () => {
+        props.setTodos(props.todos.map(el => {
+            if(el.id === props.todo.id) {
+                return {
+                    ...el, completed: !el.completed
+                }
+            }
+            return el
+        }))
+    }
     return( 
         <div className='todo'>
-            < li className='todo-item'>{props.text}</li>
-            <button className='complete-btn'><i className='fas fa-check'></i></button>
+            < li className={`todo-item ${props.todo.completed ? "completed" : ''}`}>{props.text}</li>
+            <button onClick={completeHandler} className='complete-btn'><i className='fas fa-check'></i></button>
             <button onClick={deleteHandler} className='trash-btn'><i className='fas fa-trash'></i></button>
         </div>  
     )
