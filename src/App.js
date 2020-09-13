8d979aedd676561cec2d1e368cfab33b85c936e0
krabecb <60526718+krabecb@users.noreply.github.com>
2020-09-13 10:07:01 -0500

    Added filterHandler() and utilized useEffect to update state for filtered todos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 //Importing Components
 import Form from './components/Form'
 import ToDoList from './components/ToDoList'
 
 function App() {
+  //States
   const [inputText, setInputText] = useState("")
   const [todos, setTodos] = useState([])
   const [status, setStatus] = useState('all')
+  const [filteredTodos, setFilteredTodos] = useState([])
+  //useEffect
+  //console.log is the first argument. [] is the second argument. For every time todos or status changes, 
+  //[] will activate the console log
+  useEffect(() => {
+    // console.log("hey")
+    filterHandler()
+  }, [todos, status])
+  //Functions
+  //A function that filters through the todos. If a todo's completed is true, mark it as completed
+  //If a todo's completed is false, then mark as uncompleted
+  const filterHandler = () => {
+    switch(status) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
+        break
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false))
+        break
+      default:
+        setFilteredTodos(todos)
+        break
+    }
+  }
   return (
     <div className="App">
       <header>
