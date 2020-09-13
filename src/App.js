f63f8c02c1b70dc5459ae824d28cdfbd0028d9d7
krabecb <60526718+krabecb@users.noreply.github.com>
2020-09-12 20:51:28 -0500

    Imported ToDoList into App.js. Need to set up state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 //Importing Components
 import Form from './components/Form'
+import ToDoList from './components/ToDoList'
 
 function App() {
+  
   return (
     <div className="App">
       <header>
         <h1>To-do List</h1>
       </header>
       <Form />
+      <ToDoList />
     </div>
   );
 }
